d1bb63301ffb26092c9daccb1237a7c2c1234b57
Mojzoo <godmasterzila@gmail.com>
2026-02-03 08:59:42 +0100

    baza

--- Bankarski System/Program.cs
+++ Bankarski System/Program.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 class Program
 {
+    public void Data()
+    {
+        string connection = "server = 127.0.0.1; database = mojzo; user = root; password = ;";
+        MySqlConnection conn = new MySqlConnection(connection);
+
+    }
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello");
